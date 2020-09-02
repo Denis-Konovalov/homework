8dd7c7b2e0b3231ddf4797a6914b9b68893bd152
de.konovalov <de.konovalov@x5.ru>
2020-09-02 09:15:09 +0300

    Добавил проверку исходных данных

--- src/ru/x5/homework1/Main.java
+++ src/ru/x5/homework1/Main.java
@@ -2,10 +2,39 @@ package ru.x5.homework1;
 
 /*1. Ввести целое число в консоли и вывести на экран последнюю цифру введенного числа.*/
 
+import java.sql.Array;
+
 public class Main {
     public static void main(String[] args) {
+        checkArray(args);
         String str = args[0];
-        System.out.println("Исходные данные: "+str);
-        System.out.println("Последнее число: "+str.charAt(str.length()-1));
+        System.out.println("Исходные данные: " + str);
+        isNumeric(str);
+        System.out.println("Последняя цифра: " + str.charAt(str.length() - 1));
+    }
+
+    private static void checkArray(String[] array){
+        if(array == null){
+            System.out.println("Отсутствуют исходные данные!");
+            System.exit(1);
+        }
+        if(array.length == 0){
+            System.out.println("Массив входных данных пуст!");
+            System.exit(1);
+        }
+        if(array.length > 1){
+            System.out.println("Задано больше одного числа!");
+            System.exit(1);
+        }
+    }
+
+    private static void isNumeric(String strNum) {
+        try {
+            long l = Long.parseLong(strNum);
+        } catch (NumberFormatException | NullPointerException nfe) {
+            System.out.println("Строка не является целым числом!");
+            System.exit(1);
+        }
     }
 }
+
