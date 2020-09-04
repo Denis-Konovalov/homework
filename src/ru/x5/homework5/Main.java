875cec51f33f4ffa28e025c25d1940d224bf3bc1
de.konovalov <de.konovalov@x5.ru>
2020-09-04 13:23:05 +0300

    Добавил пятое задание

---
+++ src/ru/x5/homework5/Main.java
@@ -0,0 +1,21 @@
+package ru.x5.homework5;
+
+
+/*5. Ввести три целых числа с консоли. Вывести на экран наименьшее из них.*/
+
+import ru.x5.Service;
+
+public class Main {
+    public static void main(String[] args) {
+        Service.checkArray(args, 3);
+        System.out.println("Исходные данные:");
+        int val = Integer.parseInt(args[0]);
+        for (int i = 0; i < 3; i++) {
+            System.out.println(i+1 + ") " + args[i]);
+            if (val > Integer.parseInt(args[i])) {
+                val = Integer.parseInt(args[i]);
+            }
+        }
+        System.out.println("Результат: " + val);
+    }
+}
