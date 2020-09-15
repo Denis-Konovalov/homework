00a51d43e3a77c15eda0167c22c4d3821511c617
de.konovalov <de.konovalov@x5.ru>
2020-09-15 15:40:41 +0300

    Добавил задание с классом Phone

---
+++ src/ru/x5/work3/phone/Phone.java
@@ -0,0 +1,41 @@
+package ru.x5.work3.phone;
+
+public class Phone {
+    String number;
+    String model;
+    Double weight;
+
+    Phone() {
+    }
+
+    Phone(String number, String model) {
+        this.number = number;
+        this.model = model;
+    }
+
+    Phone(String number, String model, Double weight) {
+        this(number,model);
+        this.weight = weight;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void receiveCall(String name) {
+        System.out.println("Звонит " + name);
+    }
+
+    public void receiveCall(String name, String number) {
+        this.receiveCall(name);
+    }
+
+    public void sendMessage(String[] numbers){
+        for (String val: numbers){
+            System.out.println(val);
+        }
+
+    }
+
+
+}
